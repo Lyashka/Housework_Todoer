ce606dccaf73c735413b8b34cdaf69bac5ec40aa
Lyashka <iuliana9864@gmail.com>
2020-08-23 20:14:25 +0300

    change entrance of project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
+import HomePageForParent from './HomePageForParent';
 import './style.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Hello, World!
-      </header>
+      <HomePageForParent />
     </div>
   );
 }
