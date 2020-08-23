06c9d845c869f2de6a1bd6ff31f5462848630bc7
Lyashka <iuliana9864@gmail.com>
2020-08-23 20:12:05 +0300

    add const

---
+++ src/const.js
@@ -0,0 +1,32 @@
+const colorForEasistTask = "#FFB966";
+const colorForEasyTask = "#FE8368";
+const colorForNormalTask = "#FF5F5F";
+const colorForDifficultTask = "#FF467D";
+
+export const tasks = [
+  {
+    reward: 125,
+    description: "Find dad’s wallet",
+    color: colorForDifficultTask
+  },
+  {
+    reward: 100,
+    description: "Put away old toys to white boxes on the balkoney",
+    color: colorForNormalTask
+  },
+  {
+    reward: 75,
+    description: "Wash the dishes",
+    color: colorForEasyTask
+  },
+  {
+    reward: 75,
+    description: "Wash the dishes",
+    color: colorForEasyTask
+  },
+  {
+    reward: 50,
+    description: "Fix the pillowcase",
+    color: colorForEasistTask
+  }
+];
