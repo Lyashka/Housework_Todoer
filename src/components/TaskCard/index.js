688ee8afc2232c873bf6e330cdff42db63a43e1d
Lyashka <iuliana9864@gmail.com>
2020-09-08 17:49:26 +0300

    add layout of task card in edit mode

--- src/components/TaskCard/index.js
+++ src/components/TaskCard/index.js
@@ -12,14 +12,58 @@ export default class TaskCard extends Component {
 
   render() {
     return (
-      <div className="task-card" style={{ backgroundColor: this.color }}>
+      <article className="edit-task-card">
+        <div className="edit-task-card__header">
+          <p className="edit-task-card__title">Change task</p>
+        </div>
+
+        <form className="edit-task-form">
+          <div className="edit-task-card__body">
+            <div className="edit-task-form__title">
+              <label className="description-title">Title</label>
+              <textarea
+                className="description-textarea"
+                name="description"
+                required
+                defaultValue={this.description}>
+              </textarea>
+            </div>
+            <div className="edit-task-form__reward">
+              <label className="reward-title">Reward</label>
+              <input
+                className="reward-input"
+                name="reward"
+                type="number"
+                min="50" max="250"
+                required
+                defaultValue={this.reward}>
+              </input>
+            </div>
+          </div>
+
+          <div className="edit-task-card__footer">
+            <div className="row-buttons-container">
+              <button className="cancel-button" type="button" onClick={this.handleRejectEditTask}>
+                Cancel
+              </button>
+              <button className="save-change-button" type="submit">
+                Save
+              </button>
+            </div>
+            <button className="delete-task-button" type="button" onClick={this.handleRemoveTask}>
+              Delete
+            </button>
+          </div>
+        </form>
+      </article>
+      /*<div className="task-card" style={{ backgroundColor: this.color }}>
         <header className="task-card__header">
           <p className="task-card__reward">{this.reward}</p>
         </header>
         <main className="task-card__body">
           <p className="task-card__description">{this.description}</p>
         </main>
-      </div>
+      </div>*/
     )
   }
 }
