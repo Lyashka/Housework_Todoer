3a362cdd5eee0d9cfe71e784548377c6f3d9ad32
Lyashka <iuliana9864@gmail.com>
2020-10-24 11:22:48 +0300

    fix props error

--- src/components/ChildrenPanel/index.js
+++ src/components/ChildrenPanel/index.js
@@ -4,23 +4,14 @@ import MaryImg from '../../icons/Mary.svg';
 import './childrenPanel.scss';
 
 export default class ChildrenPanel extends Component {
-  constructor(props) {
-    super(props);
-
-    this.name = this.props.name;
-    this.coinsSum = this.props.coinsSum;
-  }
-
   render() {
-    console.log(this.coinsSum)
-    const pathByName = this.name === 'Lily' ? LilyImg : MaryImg;
+    const pathByName = this.props.name === 'Lily' ? LilyImg : MaryImg;
 
     return (
-      <>
         <div className="container-child">
           <div className="child">
             <div className="child__avatar">
-              <img className="child__img" src={pathByName} alt={this.name}/>
+              <img className="child__img" src={pathByName} alt={this.props.name}/>
             </div>
 
             <div className="child__information">
@@ -31,14 +22,13 @@ export default class ChildrenPanel extends Component {
               </div>
               <div className="child__body">
                 <div className="child__text">
-                  <p className="child__greeting">{'Hello, ' + this.name + '!'}</p>
-                  <p className="child__reward">{this.coinsSum} HC</p>
+                  <p className="child__greeting">{'Hello, ' + this.props.name + '!'}</p>
+                  <p className="child__reward">{this.props.coinsSum} HC</p>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </>
     );
   }
 }
