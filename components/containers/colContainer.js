04dcdd40815a288d084b383c55264246f525d2f2
Juliana Samoylenko <yuliana.samoilenko@flatstack.com>
2020-11-27 18:21:08 +0300

    add css styles for component like mixin

---
+++ components/containers/colContainer.js
@@ -0,0 +1,8 @@
+import { css } from 'styled-components';
+
+const ColContainer = css`
+  display: flex;
+  flex-direction: column;
+`;
+
+export default ColContainer;
---
+++ components/containers/rowContainer.js
@@ -0,0 +1,8 @@
+import { css } from 'styled-components';
+
+const RowContainer = css`
+  display: flex;
+  flex-direction: row;
+`;
+
+export default RowContainer;
