4c4e5853c772d03e5b79f12d5656c78916f5632e
Juliana Samoylenko <yuliana.samoilenko@flatstack.com>
2020-12-01 18:03:25 +0300

    add content in main container

--- components/ParentPage/MainContent/index.js
+++ components/ParentPage/MainContent/index.js
@@ -1,18 +1,43 @@
+/* eslint-disable array-callback-return */
 import React from 'react';
 import styled from 'styled-components';
+import NavigationPanel from './NavigationPanel';
+import TaskCard from './TaskCard';
+import RowContainer from '../../containers/rowContainer';
+import { tasks } from '../../../const';
 
 const Content = styled.section`
   display: flex;
   flex-direction: column;
   width: 70%;
   height: 75vh;
-
   background-color: yellow;
+`;
+
+const Tasks = styled.ul`
+  ${RowContainer};
+  align-content: start;
+  margin: 0 -10px;
+  width: 100%;
+  height: 500px;
+  list-style-type: none;
+  flex-wrap: wrap;
+  overflow-y: auto;
 `;
 
 function MainContent() {
   return (
-    <Content />
+    <Content>
+      <NavigationPanel />
+      <Tasks>
+        {tasks.map(({
+          id,
+          description,
+          reward,
+          color,
+        }) => <TaskCard key={id} color={color} description={description} reward={reward} />)}
+      </Tasks>
+    </Content>
   );
 }
 
