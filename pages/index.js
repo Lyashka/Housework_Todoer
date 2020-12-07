76988f5a89639c5ab98929fbfa1f617d2a6b3090
Juliana Samoylenko <yuliana.samoilenko@flatstack.com>
2020-12-07 15:03:21 +0300

    display child page

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import ParentPage from '../components/ParentPage';
+// import ParentPage from '../components/ParentPage';
+import ChildPage from '../components/ChildPage';
 
 function App() {
   return (
     <>
-      <ParentPage user />
+      <ChildPage user />
     </>
   );
 }
